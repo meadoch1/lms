66cdffd2e65aab2bc1943b5ed04bf1281b484e04
Chris Meadows <meadoch1@gmail.com>
2013-04-02 14:48:55 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+lms
+===
+
+Last Man Standing
+
+A sample application using Ember.js and Rails
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lms::Application.config.secret_token = 'a5567d53e815aa49c6ad0d915432e5cd576a2dd216200c77901dec7d109f275bd4e0bfb94ee8dd141bb5e22fafdcb9704e0bfe85117a1a79db50a6d0e945ee16'
