64ba1bfe58ed2dd969b8d4c75284b9be487d58c4
Chris Meadows <meadoch1@gmail.com>
2013-04-04 16:36:50 -0400

    Split lists to waiting and not waiting

--- app/assets/javascripts/models/participant.js
+++ app/assets/javascripts/models/participant.js
@@ -1,4 +1,7 @@
 App.Participant = DS.Model.extend({
     name: DS.attr('string'),
-    state: DS.attr('string')
+    state: DS.attr('string'),
+    isWaiting: function() {
+        return this.get('state') === 'Waiting';
+    }.property('state')
 });
