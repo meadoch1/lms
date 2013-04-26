a74f83571b36990f4bc9d7d63f05b5dd65c37b36
Chris Meadows <meadoch1@gmail.com>
2013-04-26 10:55:47 -0400

    Ready for presentation

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,6 +1,13 @@
 App.Router.map(function() {
+	// index template will be displayed when / is visited
     this.route('index', {path: '/'});
+
+    // participants.index template will be displayed when /participants is visited
     this.resource('participants', function() {
+
+		// participants.show template will be displayed when /participants/1 is visited
         this.route('show', {path: ':participant_id' });
-    });
+    });  
+    //both participants routes above will be wrapped in the participants template since they are nested
 });
+
